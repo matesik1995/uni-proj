c8eb96701a1a5e825a29458ab8d51f844d956247
matesik1995 <matesik1995@gmail.com>
2019-05-24 21:45:19 +0200

    Add comments

--- profiler/app.py
+++ profiler/app.py
@@ -3,58 +3,63 @@ from datetime import datetime
 import argparse
 import csv
 
-
+# Dictionary of kev:value pairs, where
+#   key - is a symbol of column type
+#   value - is a query to be performed to calculate all the statistics. `%col%` is replaced with column name.
+#			Each metric should have alias in following format:
+#				%col%___STAT_NAME
+#			`___` is treated as safe delimeter between column name and calculated metric
 STATISTICS_QUERY_BY_TYPE = {
-	"I": """record_count-COUNT(CAST(%col% as BIGINT)) AS %col%_nulls, 
-			COUNT(DISTINCT CAST(%col% as BIGINT)) AS %col%_distincts,
-			MIN(CAST(%col% as BIGINT)) %col%_minimum, 
-			MAX(CAST(%col% as BIGINT)) as %col%_maximum, 
-			SUM(CAST(%col% as BIGINT)) as %col%_sum,
-			ROUND(STDDEV_POP(CAST(%col% as BIGINT)), 2) as %col%_standard_deviation, 
-			AVG(CAST(%col% as BIGINT)) as %col%_mean""", # integer
-	"F": """record_count-COUNT(%col%) AS %col%_nulls, 
-			COUNT(DISTINCT %col%) AS %col%_distincts,
-			MIN(%col%) %col%_minimum,
-			MAX(%col%) as %col%_maximum,
-			SUM(%col%) as %col%_sum, 
-			ROUND(STDDEV_POP(%col%), 2) as %col%_standard_deviation,
-			AVG(%col%) as %col%_mean""", # float
-	"D": """record_count-COUNT(%col%) AS %col%_nulls, 
-			COUNT(DISTINCT %col%) AS %col%_distincts, 
-			MIN(%col%) %col%_minimum, 
-			MAX(%col%) as %col%_maximum, 
-			SUM(%col%) as %col%_sum, 
-			ROUND(STDDEV_POP(%col%), 2) as %col%_standard_deviation,
-			AVG(%col%) as %col%_mean""", # double
-	"CF": """record_count-COUNT(%col%) AS %col%_nulls, 
-			 COUNT(DISTINCT %col%) AS %col%_distincts""", # fixed character
-	"CV": """record_count-COUNT(%col%) AS %col%_nulls,
-			 COUNT(DISTINCT %col%) AS %col%_distincts, 
-			 %col%_nulls+SUM(CASE %col% WHEN '' THEN 1 ELSE 0 END) AS %col%_empties, 
-			 SUM(CASE TRIM(%col%) WHEN '' THEN 1 ELSE 0 END) AS %col%_blanks,
-			 AVG(LENGTH(%col%)),
-			 MIN(LENGTH(%col%)),
-			 MAX(LENGTH(%col%))""", # var. character
-	"BF": """record_count-COUNT(%col%) AS %col%_nulls,
-			 COUNT(DISTINCT %col%) AS %col%_distincts""", # fixed binary
-	"BV": """record_count-COUNT(%col%) AS %col%_nulls,
-			 COUNT(DISTINCT %col%) AS %col%_distincts""", # var. binary 
-	"I2": """record_count-COUNT(CAST(%col% as BIGINT)) AS %col%_nulls, 
-			COUNT(DISTINCT CAST(%col% as BIGINT)) AS %col%_distincts,
-			MIN(CAST(%col% as BIGINT)) %col%_minimum, 
-			MAX(CAST(%col% as BIGINT)) as %col%_maximum, 
-			SUM(CAST(%col% as BIGINT)) as %col%_sum,
-			ROUND(STDDEV_POP(CAST(%col% as BIGINT)), 2) as %col%_standard_deviation, 
-			AVG(CAST(%col% as BIGINT)) as %col%_mean""", # SMALLINT
-	"DA": """record_count-COUNT(%col%) AS %col%_nulls,
-			 COUNT(DISTINCT %col%) AS %col%_distincts""", # DATE
-	"AT": """record_count-COUNT(%col%) AS %col%_nulls,
-			 COUNT(DISTINCT %col%) AS %col%_distincts""" # TIME
-	"TS": """record_count-COUNT(%col%) AS %col%_nulls,
-			 COUNT(DISTINCT %col%) AS %col%_distincts""" # TIME
+	"I": """record_count-COUNT(CAST(%col% as BIGINT)) AS %col%___nulls, 
+			COUNT(DISTINCT CAST(%col% as BIGINT)) AS %col%___distincts,
+			MIN(CAST(%col% as BIGINT)) %col%___minimum, 
+			MAX(CAST(%col% as BIGINT)) as %col%___maximum, 
+			SUM(CAST(%col% as BIGINT)) as %col%___sum,
+			ROUND(STDDEV_POP(CAST(%col% as BIGINT)), 2) as %col%___standard_deviation, 
+			AVG(CAST(%col% as BIGINT)) as %col%___mean""", # integer
+	"F": """record_count-COUNT(%col%) AS %col%___nulls, 
+			COUNT(DISTINCT %col%) AS %col%___distincts,
+			MIN(%col%) %col%___minimum,
+			MAX(%col%) as %col%___maximum,
+			SUM(%col%) as %col%___sum, 
+			ROUND(STDDEV_POP(%col%), 2) as %col%___standard_deviation,
+			AVG(%col%) as %col%___mean""", # float
+	"D": """record_count-COUNT(%col%) AS %col%___nulls, 
+			COUNT(DISTINCT %col%) AS %col%___distincts, 
+			MIN(%col%) %col%___minimum, 
+			MAX(%col%) as %col%___maximum, 
+			SUM(%col%) as %col%___sum, 
+			ROUND(STDDEV_POP(%col%), 2) as %col%___standard_deviation,
+			AVG(%col%) as %col%___mean""", # double
+	"CF": """record_count-COUNT(%col%) AS %col%___nulls, 
+			 COUNT(DISTINCT %col%) AS %col%___distincts""", # fixed character
+	"CV": """record_count-COUNT(%col%) AS %col%___nulls,
+			 COUNT(DISTINCT %col%) AS %col%___distincts, 
+			 %col%___nulls+SUM(CASE %col% WHEN '' THEN 1 ELSE 0 END) AS %col%___empties, 
+			 SUM(CASE TRIM(%col%) WHEN '' THEN 1 ELSE 0 END) AS %col%___blanks,
+			 AVG(LENGTH(%col%)) AS %col%___avg,
+			 MIN(LENGTH(%col%)) AS %col%___minimum,
+			 MAX(LENGTH(%col%)) AS %col%___maximum""", # var. character
+	"BF": """record_count-COUNT(%col%) AS %col%___nulls,
+			 COUNT(DISTINCT %col%) AS %col%___distincts""", # fixed binary
+	"BV": """record_count-COUNT(%col%) AS %col%___nulls,
+			 COUNT(DISTINCT %col%) AS %col%___distincts""", # var. binary 
+	"I2": """record_count-COUNT(CAST(%col% as BIGINT)) AS %col%___nulls, 
+			COUNT(DISTINCT CAST(%col% as BIGINT)) AS %col%___distincts,
+			MIN(CAST(%col% as BIGINT)) %col%___minimum, 
+			MAX(CAST(%col% as BIGINT)) as %col%___maximum, 
+			SUM(CAST(%col% as BIGINT)) as %col%___sum,
+			ROUND(STDDEV_POP(CAST(%col% as BIGINT)), 2) as %col%___standard_deviation, 
+			AVG(CAST(%col% as BIGINT)) as %col%___mean""", # SMALLINT
+	"DA": """record_count-COUNT(%col%) AS %col%___nulls,
+			 COUNT(DISTINCT %col%) AS %col%___distincts""", # DATE
+	"AT": """record_count-COUNT(%col%) AS %col%___nulls,
+			 COUNT(DISTINCT %col%) AS %col%___distincts""", # TIME
+	"TS": """record_count-COUNT(%col%) AS %col%___nulls,
+			 COUNT(DISTINCT %col%) AS %col%___distincts""" # TIME
 }
 
-
+# Table data class containing table name and list of it's columns
 class Table:
 	def __init__(self, name):
 		self.name = name
@@ -66,7 +71,7 @@ class Table:
 	def __repr__(self):
 		return "Table(name='{}', columns={})".format(self.name, self.columns)
 
-		
+# Column data class containg column name and it's type		
 class Column:
 	def __init__(self, name, type):
 		self.name = name
@@ -75,51 +80,63 @@ class Column:
 	def __repr__(self):
 		return "Column(name='{}', type='{}')".format(self.name, self.type)
 	
-	
+# main function	
 def main(database_name, table_names, output_fn):
-	udaExec = teradata.UdaExec()
-	with open(output_fn, 'w' ,newline='') as csvfile:
-		output = csv.writer(csvfile)
-		with udaExec.connect(method="${method}", dsn="${dsn}") as session:
-			_tables = {}
-			for row in session.execute("SELECT TRIM(TableName) as table_name, TRIM(ColumnName) as column_name, TRIM(ColumnType) as column_type FROM dbc.columns WHERE DatabaseName = '{database_name}';".format(database_name=database_name)):
-				_tables.setdefault(row.table_name, Table(row.table_name)).add_column(Column(row.column_name, row.column_type))
+	g_t1 = datetime.now()
+	udaExec = teradata.UdaExec()		# create udaExec framework entry point, load configuration from udaexec.ini
+	with open(output_fn, 'w' ,newline='') as csvfile:		# open file for output
+		output = csv.writer(csvfile)		# create output csv writer 
+		output.writerow(["Table Name", "Column Name", "Metric Name", "Metric Value", "Execution time"]) 		# write header row to csv file
+		with udaExec.connect(method="${method}", dsn="${dsn}") as session: 		# connect to TeraData instance and save connectin as session
+			_tables = {}		# temporary tables list
+			for row in session.execute("""SELECT TRIM(TableName) as table_name, TRIM(ColumnName) as column_name, TRIM(ColumnType) as column_type 
+										  FROM dbc.columns WHERE DatabaseName = '{database_name}';""".format(database_name=database_name)):
+				_tables.setdefault(row.table_name, Table(row.table_name)).add_column(Column(row.column_name, row.column_type)) # retrive all tables and it's columns from database dbc.columns
 			
 			tables = {}
-			if len(table_names) == 0:
+			if len(table_names) == 0:		# if no table_names given as argument, run calculations on all tables retrived from dbc.columns
 				tables = _tables
 			else:
-				for t in table_names:
+				for t in table_names:		# otherwise loop through given names and check if they exists in db, and select only those
 					if t not in _tables.keys():
 						raise AttributeError("Invalid input table name: '{}' is not one of {}".format(t, _tables.keys()))
 					tables[t] = _tables[t] 
 			 
-			for table in tables.values():
-				print("\n\nProcessing table: {}".format(table))
-				stmnt = "SELECT COUNT(*) as record_count, "
-				stmnt += ", ".join([STATISTICS_QUERY_BY_TYPE[column.type].replace("%col%", column.name) for column in table.columns])
+			for table in tables.values():		# main loop, processing tables one by one
+				print("\nProcessing table: {}".format(table.name))
+				#print(table)
+				stmnt = "SELECT COUNT(*) as record_count"		# prepare statement for table
+				for column in table.columns:		# each column queries are added to single statement for better performance
+					try: 
+						stmnt += ", " + STATISTICS_QUERY_BY_TYPE[column.type].replace("%col%", column.name)		# queries for statistics are chosen based on column type
+					except KeyError as e:
+						print("\t\tWarning: Unknown column type: {}".format(e))		# warning is printed if there is no query associated with column type
 				stmnt += " FROM {database_name}.".format(database_name=database_name) + table.name + ";"
 				 
-				print("\n\nCreated statement: {}".format(stmnt))
+				print("\nCreated statement. Executing...")
+				#print("Statement: {}".format(stmnt))
 				t1 = datetime.now()
-				cur = session.execute(stmnt)
+				cur = session.execute(stmnt)		#execute statement and calculate time elapsed 
 				row = cur.fetchone()
 				execution_time = datetime.now() - t1
+				print("DONE. Execution time: {}".format(execution_time))
 				
-				print("\n\nResult:")
-				print(row.columns)
-				for column in row.columns:
+				print("Writing results to file")
+				for column in row.columns:		# save results to csv file
 					try:
-						column_name, metric_name = column.split('___')
+						column_name, metric_name = column.split('___')		# split output column name by `___` which delimeters column name and metric
 					except ValueError:
 						column_name = '*'
 						metric_name = column
-					result_row = [table.name, column_name, metric_name, str(row[column]), str(execution_time)]
-					output.writerow(result_row)
-					print(result_row)
-				print("\n\nExecution time: {}".format(execution_time))
+					result_row = [table.name, column_name, metric_name, str(row[column]), str(execution_time)]		# create row for csv file. Table Name, Column Name, Metric Name, Metric Value, Execution time
+					output.writerow(result_row)		# save row
+					#print(result_row)
+					
+	print("Global execution time: {}".format(datetime.now() - g_t1))
 		
-		
+
+# program entry point
+# it defines and parse arguments and pass these to main function.		
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description='TeraDataProfiler v0.1')
 	parser.add_argument('-d', '--database', default='vmtest', help='database name')
