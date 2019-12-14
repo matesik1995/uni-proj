d766e080e7a4d58bd57ce40639a985cd92ef93ae
matesik1995 <matesik1995@gmail.com>
2019-12-14 12:34:53 +0100

    refactor date analysis, add data overview, add var relations

--- nyc-taxi/server.R
+++ nyc-taxi/server.R
@@ -8,7 +8,7 @@ library(maps)
 library(lubridate)
 
 train = read_csv("train.csv")
-#test = read_csv("test.csv")
+test = read_csv("test.csv")
 
 train <- train %>%
     mutate(pickup_datetime = ymd_hms(pickup_datetime),
@@ -17,15 +17,12 @@ train <- train %>%
            passenger_count = factor(passenger_count))
 
 shinyServer(function(input, output) {
-    set.seed(1234)
+    output$trainSummary <- renderPrint({
+        summary(train)
+    })
     
-    output$mapPlot <- renderLeaflet({
-        
-        foo <- sample_n(train, 8e3)
-        
-        leaflet(data = foo) %>% addProviderTiles("Esri.NatGeoWorldMap") %>%
-            addCircleMarkers(~ pickup_longitude, ~pickup_latitude, radius = 1,
-                             color = "blue", fillOpacity = 0.3)
+    output$testSummary <- renderPrint({
+        summary(test)
     })
     
     output$targetPlot <- renderPlot({
@@ -36,18 +33,53 @@ shinyServer(function(input, output) {
             scale_y_sqrt()
     })
     
-    output$pickupDate <- renderPlot({
+    output$targetHead <- renderPrint({
+        train %>%
+            arrange(desc(trip_duration)) %>%
+            select(trip_duration, pickup_datetime, dropoff_datetime, everything()) %>%
+            head(10)
+    })
+    
+    set.seed(1234)
+    
+    output$mapPlot <- renderLeaflet({
+        
+        foo <- sample_n(train, 1024)
+        
+        leaflet(data = foo) %>% addProviderTiles("Esri.NatGeoWorldMap") %>%
+            addCircleMarkers(~ pickup_longitude, ~pickup_latitude, radius = 1,
+                             color = "blue", fillOpacity = 0.3)
+    })
+    
+    output$pickupDateWhole <- renderPlot({
         train %>%
             ggplot(aes(pickup_datetime)) +
-            geom_histogram(fill = "red", bins = input$xxx) +
+            geom_histogram(fill = "red", bins = input$pickupDateWholeBins) +
             labs(x = "Pickup dates")
     })
     
-    output$arrivalDate <- renderPlot({
+    
+    output$pickupDateSelect <- renderPlot({
+        train %>%
+            filter(pickup_datetime > ymd(input$pickupDateFromDate) & pickup_datetime < ymd(input$pickupDateToDate)) %>%
+            ggplot(aes(pickup_datetime)) +
+            geom_histogram(fill = "red", bins = input$pickupDateSelectBins) +
+            labs(x = "Narrowed pickup dates")
+    })
+    
+    output$dropoffDateWhole <- renderPlot({
         train %>%
             ggplot(aes(dropoff_datetime)) +
-            geom_histogram(fill = "blue", bins = 120) +
+            geom_histogram(fill = "blue", bins = input$dropoffDateWholeBins) +
             labs(x = "Dropoff dates")
+    })
+    
+    output$dropoffDateSelect <- renderPlot({
+        train %>%
+            filter(dropoff_datetime > ymd(input$dropoffDateFromDate) & dropoff_datetime < ymd(input$dropoffDateToDate)) %>%
+            ggplot(aes(dropoff_datetime)) +
+            geom_histogram(fill = "blue", bins = input$dropoffDateSelectBins) +
+            labs(x = "Narrowed dropoff dates")
     })
     
     output$storeFwdFlag <- renderPlot({
@@ -72,6 +104,27 @@ shinyServer(function(input, output) {
             ggplot(aes(passenger_count, n, fill = passenger_count)) +
             geom_col() +
             scale_y_sqrt() +
+            theme(legend.position = "none")
+    })
+    
+    output$relation1_1 <- renderPlot({
+        train %>%
+            mutate(wday = wday(pickup_datetime, label = TRUE)) %>%
+            group_by(wday, vendor_id) %>%
+            summarise(median_duration = median(trip_duration)/60) %>%
+            ggplot(aes(wday, median_duration, color = vendor_id)) +
+            geom_point(size = 4) +
+            labs(x = "Day of the week", y = "Median trip duration [min]")
+    })
+    output$relation1_2 <- renderPlot({
+        train %>%
+            mutate(hpick = hour(pickup_datetime)) %>%
+            group_by(hpick, vendor_id) %>%
+            summarise(median_duration = median(trip_duration)/60) %>%
+            ggplot(aes(hpick, median_duration, color = vendor_id)) +
+            geom_smooth(method = "loess", span = 1/2) +
+            geom_point(size = 4) +
+            labs(x = "Hour of the day", y = "Median trip duration [min]") +
             theme(legend.position = "none")
     })
 })
--- nyc-taxi/ui.R
+++ nyc-taxi/ui.R
@@ -1,85 +1,131 @@
 library(shinydashboard)
+
 library(leaflet)
 library(leaflet.extras)
 
 dashboardPage(
     dashboardHeader(title = "New York City Taxi"),
-    dashboardSidebar(
-        sidebarMenu(
-            menuItem("Introduction", tabName = "introduction"),
-            menuItem("Map", tabName = "map"),
-            menuItem("Target Value", tabName = "target-value"),
-            menuItem("Date analysis", tabName = "date-analysis"),
-            menuItem("More information", tabName = "more-information")
+    dashboardSidebar(sidebarMenu(
+        menuItem("Introduction", tabName = "introduction"),
+        menuItem("Data overview",  tabName = "data-overview",
+            menuSubItem("Overview", tabName = "data-overview"),
+            menuSubItem("Target Value", tabName = "target-value"),
+            menuSubItem("Map", tabName = "map"),
+            menuSubItem("Date analysis", tabName = "date-analysis"),
+            menuSubItem("More information", tabName = "more-information")
+        ),
+        menuItem("Data relations", tabName = "to-do",
+            menuSubItem("Pickup date/time vs trip_duration", tabName = "relation1"),
+            menuSubItem("Passenger count and Vendor vs trip_duration", tabName = "to-do"),
+            menuSubItem("Store and Forward vs trip_duration", tabName = "to-do")
         )
-    ),
+    )), 
     dashboardBody(
         tabItems(
             # First tab content
-            tabItem(tabName = "introduction",
-                    h2("Introduction"),
-                    div("The competition dataset is based on the 2016 NYC Yellow Cab trip record data made available in Big Query on Google Cloud Platform. The data was originally published by the NYC Taxi and Limousine Commission (TLC). The data was sampled and cleaned for the purposes of this playground competition. Based on individual trip attributes, participants should predict the duration of each trip in the test set."),
-                    h2("File descriptions"),
-                    tags$ul(
-                        tags$li("train.csv - the training set (contains 1458644 trip records)"),
-                        tags$li("test.csv - the testing set (contains 625134 trip records)"),
-                        tags$li("sample_submission.csv - a sample submission file in the correct format")
+            tabItem(
+                tabName = "introduction",
+                h2("Introduction"),
+                div(
+                    "The competition dataset is based on the 2016 NYC Yellow Cab trip record data made available in Big Query on Google Cloud Platform. The data was originally published by the NYC Taxi and Limousine Commission (TLC). The data was sampled and cleaned for the purposes of this playground competition. Based on individual trip attributes, participants should predict the duration of each trip in the test set."
+                ),
+                h2("File descriptions"),
+                tags$ul(
+                    tags$li("train.csv - the training set (contains 1458644 trip records)"),
+                    tags$li("test.csv - the testing set (contains 625134 trip records)"),
+                    tags$li(
+                        "sample_submission.csv - a sample submission file in the correct format"
+                    )
+                ),
+                h2("Data fields"),
+                tags$ul(
+                    tags$li("id - a unique identifier for each trip"),
+                    tags$li(
+                        "vendor_id - a code indicating the provider associated with the trip record"
                     ),
-                    h2("Data fields"),
-                    tags$ul(
-                        tags$li("id - a unique identifier for each trip"),
-                        tags$li("vendor_id - a code indicating the provider associated with the trip record"),
-                        tags$li("pickup_datetime - date and time when the meter was engaged"),
-                        tags$li("dropoff_datetime - date and time when the meter was disengaged"),
-                        tags$li("passenger_count - the number of passengers in the vehicle (driver entered value)"),
-                        tags$li("pickup_longitude - the longitude where the meter was engaged"),
-                        tags$li("pickup_latitude - the latitude where the meter was engaged"),
-                        tags$li("dropoff_longitude - the longitude where the meter was disengaged"),
-                        tags$li("dropoff_latitude - the latitude where the meter was disengaged"),
-                        tags$li("store_and_fwd_flag - This flag indicates whether the trip record was held in vehicle memory before sending to the vendor because the vehicle did not have a connection to the server - Y=store and forward; N=not a store and forward trip"),
-                        tags$li("trip_duration - duration of the trip in seconds")
-                    )
-            ),
-            tabItem(tabName = "map",
-                    box(
-                        title = "Map",
-                        width = 12, 
-                        leafletOutput("mapPlot", height = 600)
-                    )        
-            ),
-            tabItem(tabName = "target-value",
-                    box(
-                        title = "Target value",
-                        width = 12,
-                        plotOutput("targetPlot")
+                    tags$li("pickup_datetime - date and time when the meter was engaged"),
+                    tags$li("dropoff_datetime - date and time when the meter was disengaged"),
+                    tags$li(
+                        "passenger_count - the number of passengers in the vehicle (driver entered value)"
                     ),
-                    box(
-                        title = "Controls",
-                        width = 12,
-                        sliderInput("bins", "Number of observations:", 100, 1000, 500)
-                    )
+                    tags$li("pickup_longitude - the longitude where the meter was engaged"),
+                    tags$li("pickup_latitude - the latitude where the meter was engaged"),
+                    tags$li("dropoff_longitude - the longitude where the meter was disengaged"),
+                    tags$li("dropoff_latitude - the latitude where the meter was disengaged"),
+                    tags$li(
+                        "store_and_fwd_flag - This flag indicates whether the trip record was held in vehicle memory before sending to the vendor because the vehicle did not have a connection to the server - Y=store and forward; N=not a store and forward trip"
+                    ),
+                    tags$li("trip_duration - duration of the trip in seconds")
+                )
             ),
-            tabItem(tabName = "date-analysis",
-                    tabBox(
-                        id = "datetabset", width = 12, 
-                        tabPanel("Pickup date", 
-                                 plotOutput("pickupDate"),
-                                 box(
-                                     title = "Date range",
-                                     width = 6,
-                                     dateInput("fromDate", "From:", value = "2012-02-29"),
-                                     dateInput("toDate", "To:", value = "2020-02-29"),
-                                 ),
-                                 box(
-                                     title = "Controls",
-                                     width = 6,
-                                     sliderInput("xxx", "Number of observations:", 1, 360, 31)
-                                 )
-                        ),
-                        tabPanel("Arrival date", 
-                                 plotOutput("arrivalDate")         
-                        )
+            tabItem(
+                tabName = "data-overview",
+                h2("Data overview"),
+                div(
+                    "Training data consists of 11 variables. There are no nulls or missing values."
+                ),
+                verbatimTextOutput("trainSummary"),
+                div(
+                    "Test data consists of 9 variables. The trip_duration and dropoff_datetime fields are missing."
+                ),
+                verbatimTextOutput("testSummary")
+            ),
+            tabItem(
+                tabName = "target-value",
+                box(title = "Target value",
+                    width = 12,
+                    plotOutput("targetPlot")),
+                box(
+                    title = "Controls",
+                    width = 12,
+                    sliderInput("bins", "Number of bins:", 10, 200, 50)
+                ),
+                div(
+                    "The majority of rides follow a rather smooth distribution that looks almost log-normal with a peak just short of 1000 seconds, i.e. about 17 minutes."
+                ),
+                div(
+                    "Additionally, there is a strange delta-shaped peak of trip_duration just before the 1e5 seconds mark and even a few way above it"
+                ),
+                verbatimTextOutput("targetHead"),
+                div(
+                    "Those records would correspond to 24-hour trips and beyond, with a maximum of almost 12 days"
+                )
+            ),
+            tabItem(
+                tabName = "map",
+                box(
+                    title = "Map",
+                    width = 12,
+                    leafletOutput("mapPlot", height = 600)
+                ),
+                div(
+                    "This map gives us a general overview of how the pickup and dropoff positions looks like."
+                )
+            ),
+            
+            tabItem(
+                tabName = "date-analysis",
+                tabBox(
+                    id = "datetabset",
+                    width = 12,
+                    tabPanel(
+                        "Pickup date",
+                        plotOutput("pickupDateWhole"),
+                        sliderInput("pickupDateWholeBins", "Number of bins:", 1, 360, 180),
+                        plotOutput("pickupDateSelect"),
+                        dateInput("pickupDateFromDate", "From:", value = "2016-01-20"),
+                        dateInput("pickupDateToDate", "To:", value = "2016-02-05"),
+                        sliderInput("pickupDateSelectBins", "Number of bins:", 1, 360, 180)
+                    ),
+                    tabPanel("Arrival date",
+                         plotOutput("dropoffDateWhole"),
+                         sliderInput("dropoffDateWholeBins", "Number of bins:", 1, 360, 180),
+                         plotOutput("dropoffDateSelect"),
+                         dateInput("dropoffDateFromDate", "From:", value = "2016-01-20"),
+                         dateInput("dropoffDateToDate", "To:", value = "2016-02-05"),
+                         sliderInput("dropoffDateSelectBins", "Number of bins:", 1, 360, 180)
                     )
+                )
             ),
             tabItem(tabName = "more-information",
                     fluidRow(
@@ -88,11 +134,9 @@ dashboardPage(
                             width = 6,
                             plotOutput("storeFwdFlag")
                         ),
-                        box(
-                            title = "Vendors",
+                        box(title = "Vendors",
                             width = 6,
-                            plotOutput("vendors")
-                        )
+                            plotOutput("vendors"))
                     ),
                     fluidRow(
                         box(
@@ -100,7 +144,19 @@ dashboardPage(
                             width = 12,
                             plotOutput("passengerCount")
                         )
-                    )          
+                    )
+            ),
+            tabItem(tabName = "relation1",
+                    plotOutput("relation1_1"),
+                    plotOutput("relation1_2"),
+                    div("We find:
+
+There is indeed a similar pattern as for the business of the day of the week. Vendor 2, the one with the more frequent trips, also has consistently higher trip durations than vendor 1. It will be worth adding the vendor_id feature to a model to test its predictive importance.
+
+Over the course of a typical day we find a peak in the early afternoon and dips around 5-6am and 8pm. The weekday and hour of a trip appear to be important features for predicting its duration and should be included in a successful model.")
+            ),
+            tabItem(tabName = "to-do",
+                    div("to do zrobionka")
             )
         )
     )
